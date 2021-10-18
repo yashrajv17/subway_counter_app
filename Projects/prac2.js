6a7453a16ffa70df4af5b5682339ab977100de93
Yash Raj Vishwanath <91608542+yashrajv17@users.noreply.github.com>
2021-10-18 18:35:18 +0530

    Delete Projects directory

--- Projects/prac2.js
+++
@@ -1,6 +0,0 @@
-let name ="Yash"
-let greet ="Hi! My name is.."
-
-let close = greet +" "+ name
-
-console.log(close)
--- Projects/script.js
+++
@@ -1,7 +0,0 @@
-let namasteBtn = document.querySelector('button');
-namasteBtn.addEventListener('click', inputMsg);
-
-function inputMsg(){
-    let name = prompt('Enter Name of the Student');
-    namasteBtn.textContent = 'Roll No. 1' + name;
-}
